401fe86999aeb7b765c0547f11c3574fbf93b99c
daniyaniazi <farihadania@hotmail.com>
2021-04-15 17:41:20 +0500

    Email Already exist validation

--- NodeJWTAuth/routes/auth.js
+++ NodeJWTAuth/routes/auth.js
@@ -6,6 +6,12 @@ router.post('/register', async (req, res) => {
     if (error) {
         return res.status(400).send(error.details[0].message)
     }
+    //if user is already in Datatbase
+    const emailExist = await User.findOne({ email: req.body.email })
+    if (emailExist) {
+        return res.status(400).send("Email already Exist")
+    }
+    //Create a new User
     const user = new User({
         name: req.body.name,
         email: req.body.email,
