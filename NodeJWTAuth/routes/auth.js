eca2a51576139e641b4b5a192a3a76cb3e84e9cd
daniyaniazi <farihadania@hotmail.com>
2021-04-15 17:58:52 +0500

    hashing password

--- NodeJWTAuth/routes/auth.js
+++ NodeJWTAuth/routes/auth.js
@@ -1,5 +1,7 @@
 const router = require("express").Router();
 const User = require('../model/User')
+const bcrypt = require("bcryptjs")
+
 const { registerValidation } = require('../validation')
 router.post('/register', async (req, res) => {
     const { error } = registerValidation(req.body)
@@ -11,16 +13,21 @@ router.post('/register', async (req, res) => {
     if (emailExist) {
         return res.status(400).send("Email already Exist")
     }
+
+    //Hash Passwords
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(req.body.password, salt)
+
     //Create a new User
     const user = new User({
         name: req.body.name,
         email: req.body.email,
-        password: req.body.password
+        password: hashedPassword
     });
 
     try {
         const savedUser = await user.save()
-        res.send(savedUser)
+        res.send({ user: user._id })
     } catch (error) {
         res.status(400).send(error)
     }
